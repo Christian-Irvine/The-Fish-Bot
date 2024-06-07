104c2bb5d27e81b8ff82db4fc736502620abd6c2
Christian Irvine <christian.irvine04@gmail.com>
2024-06-07 18:14:30 +1200

    Setup basic bot files

---
+++ src/index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const { Client, IntentsBitField } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ],
+});
+
+client.on('ready', (c) => {
+    console.log(`${c.user.tag} is ready to swim!`);
+});
+
+client.on('messageCreate', (msg) => {
+    if (msg.author.bot) return;
+    msg.reply("Howdy partner");
+});
+
+client.login(process.env.TOKEN);
